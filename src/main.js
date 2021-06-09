2a4df46b2ef864413b5ec1d7ff1ea22784058dcc
Hugo <hugointe@gmail.com>
2021-06-08 22:54:51 -0400

    [HU0][Sprint0] Se añade Sentry
    Se añade Sentry, un tracker de errores para comprobar el estado de la página a lo largo de los commits.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
+
 
 
 Vue.config.productionTip = false
+
+Sentry.init({
+  Vue,
+  dsn: "https://d264845d1b7d42b59fec48009d9517b2@o291882.ingest.sentry.io/5807501",
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
 
 new Vue({
   vuetify,
