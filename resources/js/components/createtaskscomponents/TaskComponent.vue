ad3fe9c70cef1e3b804c6bd35dc77ffb393a2e3d
Leunam <manuelvaalram@gmail.com>
2021-06-20 20:51:24 -0400

    Implementación diseño inicial creador de tareas
    Se implementó medianamente el diseño propuesto

--- resources/js/components/createtaskscomponents/TaskComponent.vue
+++ resources/js/components/createtaskscomponents/TaskComponent.vue
@@ -45,41 +45,63 @@
 
     <!-- Botón o tarjeta de cada tarea -->
     <v-card
+      class="white--text"
+      color="#73009d"
       @click="dialog = true"
-      class="mt-5 mb-5"
     >
-      <v-card-title>{{ taskData.name }}</v-card-title>
-      <v-row>
-        <v-col
-          cols="1"
-          v-for="(member, index) in taskData.members"
-          :key="index"
-        >
-          <v-tooltip top>
-            <template v-slot:activator="{ on }">
-              <v-btn
-                fab
-                small
-                v-on="on"
-              >
-                {{ member.charAt(0) }}
-              </v-btn>
-            </template>
-            <span>{{ member }}</span>
-          </v-tooltip>
-        </v-col>
-      </v-row>
+      <v-card-title
+        class="subheading font-weight-bold"
+      >
+        {{ taskData.name }}
+      </v-card-title>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-content>
+            Descripción:
+          </v-list-item-content>
+          <v-list-item-content
+            class="align-end"
+          >
+            {{taskData.desc.slice(0, 20)+'...'}}
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            Miembros:
+          </v-list-item-content>
+          <v-list-item-content
+            class="align-end"
+          >
+            {{taskData.members.toString().slice(0, 15)+'...'}}
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            Fecha:
+          </v-list-item-content>
+          <v-list-item-content
+            class="align-end"
+          >
+            {{taskData.date}}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
     </v-card>
   </div>
 </template>
+
 <script>
-export default {
-  name: 'Task',
-  data: () => ({
-    dialog: false
-  }),
-  props: {
-    taskData: null
+  export default {
+    name: 'Task',
+    data: () => ({
+      dialog: false
+    }),
+    props: {
+      taskData: null
+    }
   }
-}
 </script>
--- resources/js/components/createtaskscomponents/TaskListComponent.vue
+++ resources/js/components/createtaskscomponents/TaskListComponent.vue
@@ -1,57 +1,135 @@
 <template>
-  <v-container>
-    <div class="text-center">
-      <v-dialog v-model="dialog">
+  <v-container fluid>
+      <!--Barra de tareas-->
+      <v-toolbar
+        dark
+        color="#a40056"
+        class="mb-1"
+      >
+        <v-btn
+          color="#db0082"
+          dark
+          @click="dialog = true"
+
+        >
+          Nueva tarea
+        </v-btn>
+      </v-toolbar>
+
+    <!--Cuadro de diálogo para crear nueva tarea-->
+      <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="600px"
+      >
         <v-card>
-          <v-card-title>Crear nueva tarea</v-card-title>
-          <v-form>
-            <v-card-text>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field
-                    v-model="taskName"
-                    label="Nombre de la tarea"
-                  >
-                  </v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    v-model="taskMember"
-                    label="Miembro"
-                  >
-                  </v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-btn @click="addMember">Agregar miembro</v-btn>
-                  <v-row>
-                    <v-col
-                      cols="1"
-                      v-for="(member, index) in taskMembers"
-                      :key="index"
-                    >
-                      {{ member }}
-                    </v-col>
-                  </v-row>
-                </v-col>
-                <v-col cols="12">
-                  <v-textarea v-model="taskDesc">Descripción</v-textarea>
-                </v-col>
-                <v-col cols="12">
-                  <v-btn @click="createTask">Crear</v-btn>
-                </v-col>
-              </v-row>
-            </v-card-text>
-          </v-form>
+          <v-toolbar
+            color="#a40056"
+            dark
+          >
+            Crear nueva tarea
+          </v-toolbar>
+
+          <!--Campos de texto-->
+          <div>
+            <v-col justify="center">
+              <v-text-field
+                v-model="taskName"
+                label="Nombre"
+                hide-details="auto"
+              ></v-text-field>
+              <v-textarea
+                v-model="taskDesc"
+                label="Description"
+              ></v-textarea>
+              <v-combobox
+                v-model="taskMembers"
+                :items="peopleNames"
+                label="Miembros disponibles"
+                multiple
+                chips
+              >
+              </v-combobox>
+            </v-col>
+          </div>
+
+          <!--Seleccionador de fecha-->
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="taskDate"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="taskDate"
+                label="Picker in menu"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="taskDate"
+              no-title
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                color="primary"
+                @click="menu = false"
+              >
+                Cancelar
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.menu.save(taskDate)"
+              >
+                Guardar
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
+          <v-spacer></v-spacer>
+
+          <!--Botones del cuadro de diálogo-->
+          <v-card-actions class="justify-end">
+            <v-btn
+              text
+              @click="dialog = false"
+            >
+              Cerrar
+            </v-btn>
+            <v-btn
+              text
+              @click="createTask"
+            >
+              Guardar
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </v-dialog>
-    </div>
-    <v-card width="500">
-      <Task v-for="(task, index) in tasks"
-            :taskData="task"
-            :key="index"
-      />
-    </v-card>
-    <v-btn @click="dialog = true">Agregar tarea</v-btn>
+
+    <!--Muestra las tareas disponibles-->
+    <v-row>
+      <v-col
+        v-for="(task, index) in tasks"
+        :key="index"
+        cols="12"
+        sm="6"
+        md="6"
+        lg="4"
+      >
+        <Task
+          :taskData="task"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -63,45 +141,49 @@ export default {
   name: 'TaskList',
   components: {Task},
   data: () => ({
+    taskDate: new Date().toISOString().substr(0, 10),
+    menu: false, //Para el seleccionador de fecha
     dialog: false,
     taskName: '',
-    taskMember: '',
-    taskMembers: [],
     taskDesc: '',
+    taskMembers: [],
+    peopleNames: ['Juanito Pérez', 'Juliana Soza', 'Juancho Silva', 'Manuel Hernandez', 'Jesus Alberga', 'Pedro Perez'],
+
     tasks: [
       {
         name: 'Crear interfaz',
         members: ['Juanito Pérez', 'Juliana Soza'],
-        desc: 'Lorem ipsum dolor sit amet tempus penatibus taciti feugiat cras fames laoreet bibendum ligula nibh. Tristique convallis leo nibh porta odio feugiat blandit ullamcorper scelerisque cursus, luctus aptent netus sagittis egestas quis felis pulvinar ut vestibulum, ante mi cum suspendisse ornare potenti praesent eleifend varius. Quis dignissim dictum imperdiet bibendum mattis, vivamus phasellus donec tempor.'
+        desc: 'Lorem ipsum dolor sit amet tempus penatibus taciti feugiat cras fames laoreet bibendum ligula nibh. Tristique convallis leo nibh porta odio feugiat blandit ullamcorper scelerisque cursus, luctus aptent netus sagittis egestas quis felis pulvinar ut vestibulum, ante mi cum suspendisse ornare potenti praesent eleifend varius. Quis dignissim dictum imperdiet bibendum mattis, vivamus phasellus donec tempor.',
+        date: '25/06/2021'
       },
       {
         name: 'Implementar botones',
         members: ['Juancho Silva', 'Manuel Hernandez', 'Jesus Alberga'],
-        desc: 'Lorem ipsum dolor sit amet consectetur adipiscing Tristique egestas quis felis pulvinar ut vestibulum, ante mi cum suspendisse ornare potenti praesent eleifend varius. Quis dignissim dictum imperdiet bibendum mattis, vivamus phasellus donec tempor.'
+        desc: 'Lorem ipsum dolor sit amet consectetur adipiscing Tristique egestas quis felis pulvinar ut vestibulum, ante mi cum suspendisse ornare potenti praesent eleifend varius. Quis dignissim dictum imperdiet bibendum mattis, vivamus phasellus donec tempor.',
+        date: '10/06/2021'
       },
       {
         name: 'Seleccionar colores',
         members: ['Pedro Perez'],
-        desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit senectus fringilla arcu a, iaculis sodales magna sollicitudin ridiculus tempus penatibus facilisis ac cursus nullam praesent, venenatis lectus taciti feugiat cras fames laoreet bibendum ligula nibh. Tristique convallis leo nibh porta odio feugiat blandit ullamcorper scelerisque cursus, luctus aptent netus sagittis egestas quis felis pulvinar ut vestibulum, ante mi cum suspendisse ornare potenti praesent eleifend varius. Quis dignissim dictum imperdiet bibendum mattis, vivamus phasellus donec tempor.'
+        desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit senectus fringilla arcu a, iaculis sodales magna sollicitudin ridiculus tempus penatibus facilisis ac cursus nullam praesent, venenatis lectus taciti feugiat cras fames laoreet bibendum ligula nibh. Tristique convallis leo nibh porta odio feugiat blandit ullamcorper scelerisque cursus, luctus aptent netus sagittis egestas quis felis pulvinar ut vestibulum, ante mi cum suspendisse ornare potenti praesent eleifend varius. Quis dignissim dictum imperdiet bibendum mattis, vivamus phasellus donec tempor.',
+        date: '01/07/2021'
       }
 
     ]
   }),
   methods: {
-    addMember: function () {
-      this.taskMembers.push(this.taskMember);
-      this.taskMember = '';
-    },
     createTask: function () {
       this.tasks.push({
         name: this.taskName,
         members: this.taskMembers,
-        desc: this.taskDesc
+        desc: this.taskDesc,
+        date: this.taskDate
+
       });
 
       this.taskName = '';
-      this.taskMember = '';
-      this.taskMembers = [];
+      this.taskDesc = '';
+      this.taskMembers = '';
       this.taskDesc = '';
       this.dialog = false;
     }
