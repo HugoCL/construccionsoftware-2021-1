68d2b3ca116c34b3f37bb89c8fdc706bae27a7b9
Emilio Silva <emilio.silva1@hotmail.com>
2021-06-21 15:26:03 -0400

    Se anade campo descripcion al formulario para crear proyectos

--- resources/js/components/AddProyectComponent.vue
+++ resources/js/components/AddProyectComponent.vue
@@ -17,6 +17,13 @@
         required
         ></v-text-field>
 
+        <v-textarea
+          v-model="description"
+          label="Descripcion del proyecto"
+          class="my-2"
+          required
+        ></v-textarea>
+
          <v-date-picker
          color="#000000"
          full-width
@@ -64,17 +71,13 @@
       name: '',
       dates: ['2019-09-10', '2019-09-20'],
       select: null,
+      description: '',
       bosses: [],
       workers: [],
       items: [
         'juan',
         'lopez',
         'ramirez'
-      ],
-
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
       ],
     }),
 
@@ -83,6 +86,7 @@
         console.log(
           {
             'name':this.name,
+            'description':this.description,
             'dates':this.dates,
             'bosses':this.bosses,
             'workers':this.workers
