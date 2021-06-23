56fad36f2ccd17830978486f89fee9a0d9a00a8f
Daniel <daniel.ubilla.c@gmail.com>
2021-06-22 21:27:38 -0400

    [HU02][TA03] creacion script php Trabaja
    se creo el script que crea la tabla trabaja, que guarda los usuarios que se hacen cargo de cada tarea y el tiempo que ocupan

---
+++ app/Http/Controllers/TrabajaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trabaja;
+use Illuminate\Http\Request;
+
+class TrabajaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Trabaja  $trabaja
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trabaja $trabaja)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Trabaja  $trabaja
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trabaja $trabaja)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Trabaja  $trabaja
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trabaja $trabaja)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Trabaja  $trabaja
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trabaja $trabaja)
+    {
+        //
+    }
+}
