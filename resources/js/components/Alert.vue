3f4787a30d02259a3b2fca5169fb7202caea2150
dlwlrma23 <caortega16@alumnos.utalca.cl>
2021-06-22 21:11:30 -0400

    Componente extra de Alert para todas las páginas de la plantilla general

---
+++ resources/js/components/Alert.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <v-alert
+      border="top"
+      colored-border
+      type="info"
+      elevation="2"
+    >
+      Vestibulum ullamcorper mauris at ligula. Nam pretium turpis et arcu. Ut varius tincidunt libero. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Morbi nec metus.
+    </v-alert>
+  </div>
+</template>
