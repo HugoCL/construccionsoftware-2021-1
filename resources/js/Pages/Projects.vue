49a86387c6799b606f16d4c6c4ae8223c2c4a706
dlwlrma23 <caortega16@alumnos.utalca.cl>
2021-06-22 21:30:05 -0400

    Se agrega contenido a la pagina project
    Se usa propuesta de esilva

--- resources/js/Pages/Projects.vue
+++ resources/js/Pages/Projects.vue
@@ -1,6 +1,97 @@
 <template>
-    <div>
-        <h1>Proyectos</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tristique egestas cursus. Vestibulum aliquam sapien vel quam luctus, sit amet mollis risus aliquam. Proin dignissim justo magna, id rutrum neque cursus sed. Nunc malesuada molestie justo, sed lacinia lorem egestas a. Donec euismod diam non sodales tempus. Vivamus dui neque, aliquam eget imperdiet ac, feugiat eget massa. Mauris sit amet ex eget felis fringilla fringilla. Suspendisse potenti. Nunc blandit scelerisque lacinia. Morbi sed placerat orci, ut malesuada justo. Cras felis dui, cursus sit amet lectus sed, placerat bibendum metus. Proin in iaculis odio, sit amet consectetur arcu. Nunc facilisis nunc at maximus efficitur. Vestibulum bibendum ex sed tellus commodo, in pellentesque odio efficitur. Cras dignissim nisi odio, non rutrum ante rutrum sed.</p>
+
+<div class="row justify-content-center">
+
+
+    <div class="col-6 align-self-center">
+
+        <h1 class="text-center font-weight-light">Nuevo proyecto </h1>
+        <h3>Propuesta por Emilio </h3>
+
+        <v-form
+        ref="form"
+        lazy-validation
+    >
+        <v-text-field
+        v-model="name"
+        label="Nombre del proyecto"
+        required
+        ></v-text-field>
+
+         <v-date-picker
+         color="#000000"
+         full-width
+          v-model="dates"
+          range
+          class="my-4"
+        ></v-date-picker>
+
+        <v-select
+          v-model="bosses"
+          :items="items"
+          label="Jefe/s de proyecto"
+          multiple
+          dense
+          class="my-2"
+          clearable
+          chips
+        ></v-select>
+
+        <v-select
+          v-model="workers"
+          :items="items"
+          label="Empleados del proyecto"
+          multiple
+          dense
+          class="my-2"
+          clearable
+          chips
+        ></v-select>
+
+        <v-btn
+          class="btn w-100"
+          @click="send"
+          >
+          Enviar
+        </v-btn>
+        </v-form>
     </div>
+</div>
+
 </template>
+<script>
+  export default {
+    data: () => ({
+      name: '',
+      dates: ['2019-09-10', '2019-09-20'],
+      select: null,
+      bosses: [],
+      workers: [],
+      items: [
+        'juan',
+        'lopez',
+        'ramirez'
+      ],
+
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+    }),
+
+    methods: {
+      send () {
+        console.log(
+          {
+            'name':this.name,
+            'dates':this.dates,
+            'bosses':this.bosses,
+            'workers':this.workers
+          }
+        )
+      }
+    },
+  }
+</script>
+
+
