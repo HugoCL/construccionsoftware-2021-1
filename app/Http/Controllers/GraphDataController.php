ff024055ba1e8353f8658356c8e46dd0fd0d37ad
Felipe Durán <fduran16@alumnos.utalca.cl>
2021-06-28 00:10:38 -0400

    Functions
    funciones de proyectos de usuario y tareas de un proyecto

--- app/Http/Controllers/GraphDataController.php
+++ app/Http/Controllers/GraphDataController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Graph_data;
+use App\Models\Proyecto;
+use App\Models\User;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class GraphDataController extends Controller
 {
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
     /**
      * Display a listing of the resource.
      *
@@ -34,7 +45,11 @@ class GraphDataController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $graphData = new Graph_data();
+        $graphData->id = $request->id_graphData;
+
+        $graphData->save();
+        return $graphData;
     }
 
     /**
@@ -80,5 +95,36 @@ class GraphDataController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    /**
+     * This function is returning the specific project by one user
+     *
+     * @param $id_user
+     * @param $id_project
+     * @return mixed|null
+     */
+    public function userProject($id_user, $id_project)
+    {
+        $user = Usuario::find($id_user); //<- Eloquent
+        $userProjectList = $user->leadProject(); //obtiene los proyectos que dirije el usuario
+        foreach($userProjectList as $project)
+        {
+            if($project->id == $id_project)
+                return $project;
+            else
+                return null;
+        }
+    }
+
+    public function projectTasks($id_project)
+    {
+        $project = Proyecto::find($id_project); //<- Eloquent
+        //return $project->getTasks(); <-- retornar todas las tareas de un proyecto
+    }
+
+    public function taskUsers($id_task)
+    {
+
     }
 }
