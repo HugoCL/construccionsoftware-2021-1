4d4c6ad1c29cec343aa351027d714fe26dec262a
José Avendaño <javendano16@alumnos.utalca.cl>
2021-06-29 16:49:08 -0400

    Fix Fast-Forward y Cambios menores
    Se agrega como componente la vista que contiene tabla de estudiantes realizada en  sprint anterior (uso opcional)

--- resources/js/components/AdminProyectComponent.vue
+++ resources/js/components/AdminProyectComponent.vue
@@ -2,14 +2,13 @@
     <div class="container mx-0 px-0" data-app style="background-color: transparent">
         <v-container>
             <v-row >
-                <v-col cols="3" class="title mr-15 ml-1 pr-0 pb-0">
+                <v-col cols="3" class="title mr-15 ml-1 pr-0 pb-0 ">
                     Administrar proyectos
                 </v-col>
                 <v-col cols="2">
                     <v-dialog transition="dialog-top-transition"  width="64%">
                         <template v-slot:activator="{ on, attrs }">
                             <div>
-
                                 <v-btn v-bind="attrs" color="secondary" v-on="on"> <v-icon class="pr-2" >mdi-plus</v-icon>Nuevo Proyecto</v-btn>
                             </div>
                         </template>
@@ -47,7 +46,7 @@
                 <v-col cols="6">
                     <ul class="list-group mx-0 px-0">
                         <li class="list-group-item mb-4"
-                            v-for="(proyect,index) in proyectos":key="index">
+                            v-for="(proyect,index) in proyectos" :key="index">
                             <proyect-card
                                 :proyectData="proyect"
                              > </proyect-card>
