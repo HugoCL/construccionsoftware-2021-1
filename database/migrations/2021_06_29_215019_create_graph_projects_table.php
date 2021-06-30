f1f70c360738033da359b94f1d23a6ddb1a6f2cd
Fernanda <fernanda.matera.a@gmail.com>
2021-06-29 20:14:06 -0400

    Script Grafico Proyecto

---
+++ database/migrations/2021_06_29_215019_create_graph_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGraphProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('graph_projects', function (Blueprint $table) {
+            $table->integer('id');
+            $table->foreign('id_proyecto') -> references('id') -> on('proyectos');
+            $table->foreign('id_tarea') -> references('id') -> on('tarea');
+            $table->foreign('estado_proyecto') -> references('estado') -> on('proyecto');
+            $table->foreign('estado_tarea') -> references('estado') -> on('tarea');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('graph_projects');
+    }
+}
