321cbb55fd4d71d34e82a197444582ab2364cf51
isanfurg <sanfurgoH@gmail.com>
2021-07-03 21:33:22 -0400

    [HU08][TA02] Se cambia la creación del proyecto
    - Se cambia selección a fecha de inicio
    - Se selecciona tipo de metodología del proyecto
    - Se selecciona cantidad de repeticiones
    - Se selecciona el tipo de medición para las repeticiones
    - Se selecciona la duración de las repeticiones

--- resources/js/components/AddProyectComponent.vue
+++ resources/js/components/AddProyectComponent.vue
@@ -16,6 +16,8 @@
                             label="Nombre del proyecto"
                             required
                             outlined
+                            prepend-icon="mdi-fountain-pen-tip"
+
 
                         ></v-text-field>
 
@@ -24,9 +26,10 @@
                             label="Descripción del proyecto"
                             required
                             outlined
+                            prepend-icon="mdi-book-open-variant"
                             auto-grow
                         ></v-textarea>
-
+                        <!--Jefes de proyecto-->
                         <v-select
                             item-text="correo"
                             v-model="proyecto.bosses"
@@ -39,8 +42,9 @@
                             chips
                             small-chips
                             outlined
+                            prepend-icon="mdi-account-hard-hat"
                         ></v-select>
-
+                        <!--Miembros del proyecto-->
                         <v-select
                             item-text="correo"
                             v-model="proyecto.workers"
@@ -52,63 +56,83 @@
                             chips
                             small-chips
                             outlined
-                        ></v-select>
-                        <v-menu
-                            ref="menu"
-                            v-model="menu"
-                            :close-on-content-click="false"
-                            :return-value.sync="proyecto.dates"
-                            transition="scale-transition"
-                            offset-y
-                            min-width="auto"
+                            prepend-icon="mdi-account-group"
 
+                        ></v-select>
+
+                        <!--Fecha inicio/Iteracion-->
+                        <v-dialog
+                            v-model="dialogDate"
+                            persistent
+                            width="290px"
                         >
-                        <template v-slot:activator="{ on, attrs }">
+                            <template v-slot:activator="{ on, attrs }">
                                 <v-text-field
-                                    class="pl-2"
-                                    v-model="proyecto.dates"
-                                    label="Rango de fechas"
+                                    v-model="backUpDate"
+                                    label="Fecha de Inicio Proyecto"
                                     prepend-icon="mdi-calendar"
                                     readonly
+                                    outlined
+                                    class="pt-2"
                                     v-bind="attrs"
                                     v-on="on"
-                                    outlined
                                 ></v-text-field>
                             </template>
                             <v-date-picker
-                                v-model="proyecto.dates"
-                                no-title
-                                range
+                                locale="es-cl"
+                                v-model="backUpDate"
                                 scrollable
                             >
                                 <v-spacer></v-spacer>
                                 <v-btn
                                     text
                                     color="primary"
-                                    @click="menu = false"
+                                    @click="cancelDate"
                                 >
-                                    Cancelar
+                                    Cancel
                                 </v-btn>
                                 <v-btn
                                     text
                                     color="primary"
-                                    @click="$refs.menu.save(proyecto.dates)"
+                                    @click="selectDate"
                                 >
-                                    Guardar
+                                    OK
                                 </v-btn>
                             </v-date-picker>
-                        </v-menu>
-                        <!--Duracion Spring-->
+                        </v-dialog>
+                        <!--Tipo de proyecto-->
+                        <v-select
+                            outlined
+                            :items="typesProject"
+                            label="Tipo de proyecto"
+                            hint="Seleccione metodologia"
+                            v-model="proyecto.projectType"
+                            prepend-icon="mdi-folder-account-outline"
+                        >
+                        </v-select>
+                        <!--Config Duracion-->
                         <v-row>
+                            <v-col cols="4" md="4" sm="12">
+                                <!--Cantidad de repeticiones-->
+                                <v-select
+                                    outlined
+                                    :items="values"
+                                    label="Cantidad"
+                                    hint="Seleccione numero"
+                                    v-model="proyecto.projectReps"
+                                    prepend-icon="mdi-calendar-plus"
+                                >
+
+                                </v-select>
+                            </v-col>
                             <!--Formato Medicion-->
                             <v-col cols="4" md="4" sm="12">
                                 <v-select
                                     outlined
-                                    :items="rangeSprint"
-                                    label="Duracion Spring"
+                                    :items="ranges"
+                                    label="Duracion"
                                     hint="Seleccione dia/semana/mes"
                                     v-model="proyecto.rangeType"
-                                    @click="checkValuesSprint"
                                     prepend-icon="mdi-calendar-range"
                                 >
 
@@ -118,8 +142,8 @@
                             <v-col>
                                 <v-select
                                     outlined
-                                    :items="valuesSprint"
-                                    label="Valor Spring"
+                                    :items="values"
+                                    label="Tiempo"
                                     hint="Seleccione duracion"
                                     v-model="proyecto.rangeVal"
                                     prepend-icon="mdi-calendar-clock"
@@ -178,16 +202,21 @@ export default {
             proyectos:[],
             proyecto: {
                 name: '',
-                dates: [],
+                startDate:'',
                 description: '',
                 bosses: [],
                 workers: [],
+                projectType:'',
+                projectReps:'',
                 rangeType:'',
                 rangeVal:'',
                 select: null,
             },
-            rangeSprint:['Dia','Semana','Mes'],
-            valuesSprint:[],
+            backUpDate:'',
+            dialogDate:true,
+            typesProject:['Agil - Sprint [Historias de usuario]','Tradicional - Iteracion [Requisitos de sistema]'],
+            ranges:['Dia','Semana','Mes'],
+            values:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
             emailRules: [
                 v => !!v || 'E-mail is required',
                 v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
@@ -214,28 +243,6 @@ export default {
 
     methods: {
         send () {
-            if(this.proyecto.name.trim() === '' || this.proyecto.dates.length === 0 || this.proyecto.description.trim() === ''
-                || this.proyecto.bosses.length === 0 || this.proyecto.workers.length === 0){
-                alert('Debes completar todos los campos antes de guardar');
-                return;
-            }
-
-            const d1 = new Date(this.proyecto.dates[0]);
-            const d2 = new Date(this.proyecto.dates[1]);
-
-            if (+d1 >= +d2){
-                console.log(this.proyecto.dates[0] + '-' + this.proyecto.dates[1])
-                if (+d1 === +d2) {
-                    alert('Las fechas no pueden ser iguales');
-                    return;
-                }
-                let aux = this.proyecto.dates[1];
-
-                this.proyecto.dates[1] = this.proyecto.dates[0];
-                this.proyecto.dates[0] = aux;
-
-            }
-
             console.log(
                 {
                     'name':this.proyecto.name,
@@ -252,19 +259,15 @@ export default {
                 });
             window.location.href="http://127.0.0.1:8000/administrar-proyectos";
         },
-        checkValuesSprint(){
-            if (this.proyecto.rangeType==='Dia')
-                this.valuesSprint=[1,2,3,4,5,6]
-            else if (this.proyecto.rangeType==='Semana')
-                this.valuesSprint=[1,2,3,4]
-            else if (this.proyecto.rangeType==='Mes')
-                this.valuesSprint=[1,2,3,4,5,6,7,8,9,9,10,11,12]
-            else
-                this.valuesSprint=[]
-
-
-
+        cancelDate(){
+            this.backUpDate = this.proyecto.startDate
+            this.dialogDate=false
+        },
+        selectDate(){
+            this.proyecto.startDate = this.backUpDate
+            this.dialogDate=false
         }
+
     }
 }
 </script>
