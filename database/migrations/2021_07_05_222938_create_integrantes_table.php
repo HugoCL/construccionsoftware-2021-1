be6271719d0bfa175be78158b66519ed2f496041
Maximiliano Viveros <max.antonio.viveros@gmail.com>
2021-07-05 18:51:38 -0400

    avances en HU09-TA01 backend equipos

---
+++ database/migrations/2021_07_05_222938_create_integrantes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIntegrantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('integrantes', function (Blueprint $table) {
+            $table->integer('id_equipo');
+            $table->integer('id_proyecto');
+            $table->string('id_user');
+            $table->string('rol');
+            $table->foreign('id_user')->references('correo')->on('usuarios')->cascadeOnDelete();
+            $table->foreign('id_proyecto')->references('id')->on('proyectos')->cascadeOnDelete();
+            $table->foreign('id_equipo')->references('id')->on('equipo')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('integrantes');
+    }
+}
