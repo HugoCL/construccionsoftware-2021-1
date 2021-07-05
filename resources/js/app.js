c21bd5f82ae683c7a873234c46118a761519a1a2
Daniel <daniel.ubilla.c@gmail.com>
2021-07-05 02:32:28 -0400

    [HU06-TA03] creacion del kanban
    implementacion del "cascaron" del kanban en conjunto con su menu, faltan las configuraciones

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,7 +18,7 @@ Vue.component('proyect-card', require('./components/ProyectCardComponent.vue').d
 Vue.component('admin-proyects', require('./components/AdminProyectComponent.vue').default);
 Vue.component('menu-kanban',require('./components/MenuKanban.vue').default);
 Vue.component('integrantes-proyectos', require('./components/IntegrantsComponent.vue').default);
-
+Vue.component('kanban',require('./components/KanbanComponent.vue').default);
 Vue.component('header-app', require('./components/Header.vue').default);
 Vue.component('navdrawer-app', require('./components/NavDrawer.vue').default);
 Vue.component('footer-app', require('./components/Footer.vue').default);
