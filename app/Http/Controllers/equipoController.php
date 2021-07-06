7392c404fa771abbd4458b4309bfdad1f37e72e6
Felipe Durán <fduran16@alumnos.utalca.cl>
2021-07-06 15:10:38 -0400

    agregando metodo update al controlador de equipos

--- app/Http/Controllers/equipoController.php
+++ app/Http/Controllers/equipoController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Equipo;
+use App\Models\Proyecto;
 use Illuminate\Http\Request;
 
 class equipoController extends Controller
 {
-    //
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return mixed
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $equipo = Equipo::find($id);
+        $equipo->nombre = $request->nombre;
+        $equipo->id_project = $request->id_project;
+        $equipo->save();
+
+        return $equipo;
+    }
 }
