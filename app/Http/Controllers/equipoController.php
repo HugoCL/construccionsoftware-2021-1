f01062472688db8020439d9761f1c3992df4d8ee
Felipe Durán <fduran16@alumnos.utalca.cl>
2021-07-06 17:50:55 -0400

    Fixed team integrants

--- app/Http/Controllers/equipoController.php
+++ app/Http/Controllers/equipoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Equipo;
+use App\Models\Integrante;
 use App\Models\Proyecto;
 use Illuminate\Http\Request;
 
@@ -14,10 +15,11 @@ class equipoController extends Controller
      * @param $id
      * @return mixed
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, $integrantes)
     {
         //
         $equipo = Equipo::find($id);
+        $equipo->integrantes = $integrantes;
         $equipo->nombre = $request->nombre;
         $equipo->id_project = $request->id_project;
         $equipo->save();
