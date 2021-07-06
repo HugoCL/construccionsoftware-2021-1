66629798ee26d6a1b1de70c482299116d989c223
Eddea <carias14@alumnos.utalca.cl>
2021-07-06 12:08:17 -0400

    [HU09][Sprint04] Crud 'equipoController' function edit

--- app/Http/Controllers/equipoController.php
+++ app/Http/Controllers/equipoController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Equipo;
 use Illuminate\Http\Request;
 
 class equipoController extends Controller
 {
-    //
+    public function edit($id){
+        $equipo = Equipo::find($id);
+        return view('AddProyectComponent')->with($equipo);
+    }
 }
