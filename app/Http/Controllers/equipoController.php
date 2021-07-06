3942c94164bf527498bf6c012226571d2713ff1a
Fernanda <fernanda.matera.a@gmail.com>
2021-07-06 18:10:49 -0400

    HU009- TA002
    Creacion Destroy

---
+++ app/Http/Controllers/equipoController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipo;
+use App\Models\Integrante;
+use App\Models\Proyecto;
+use Illuminate\Http\Request;
+
+class equipoController extends Controller
+{
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return mixed
+     */
+    public function update(Request $request, $id, $integrantes)
+    {
+        //
+        $equipo = Equipo::find($id);
+        $equipo->integrantes = $integrantes;
+        $equipo->nombre = $request->nombre;
+        $equipo->id_project = $request->id_project;
+        $equipo->save();
+
+        return $equipo;
+    }
+
+    public function destroy($id)
+    {
+        if($id== auth()->id()){
+            $equipo = Equipo::find($id);
+            $equipo->delete();
+        }else{
+            return view ('home');
+        }
+
+    }
+}
