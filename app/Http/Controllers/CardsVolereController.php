91da435de975b98cf3acfb3c0cf5aeddc08af29f
Andres <awallberg18@alumnos.utalca.cl>
2021-07-06 21:36:13 -0400

    [HU05][TA01] CONTROLLER PT-1
    Creacion de la tabla y controller con models

---
+++ app/Http/Controllers/CardsVolereController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cards_volere;
+use Illuminate\Http\Request;
+
+class CardsVolereController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\cards_volere  $cards_volere
+     * @return \Illuminate\Http\Response
+     */
+    public function show(cards_volere $cards_volere)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\cards_volere  $cards_volere
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(cards_volere $cards_volere)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\cards_volere  $cards_volere
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, cards_volere $cards_volere)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\cards_volere  $cards_volere
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(cards_volere $cards_volere)
+    {
+        //
+    }
+}
