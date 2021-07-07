686a21f71eebbd20caa8bf2e2d481a9fb9550cf2
Cristobal <cristobal.trujillo98@gmail.com>
2021-07-06 22:00:31 -0400

    Fron CRUD HU09-TA06

--- resources/js/components/AdminProyectComponent.vue
+++ resources/js/components/AdminProyectComponent.vue
@@ -31,7 +31,6 @@
                                                     </span>
                                         </v-col>
                                     </v-row>
-
                                 </v-toolbar>
 
 
